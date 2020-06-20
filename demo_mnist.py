e256bcaf084bf358f066d109257a6a4ff17f589c
EtienneTho <etiennethoret@gmail.com>
2020-06-20 19:10:00 +0200

    First commit

---
+++ demo_mnist.py
@@ -0,0 +1,110 @@
+# Basic functions for probing classifiers with reverse correlation and bubbles
+# demo with mnist
+#
+# Written by Etienne Thoret (2020)
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from keras.models import model_from_json
+from lib import proise
+
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+categories = range(10)
+print(x_train.shape)
+print(x_test.shape)
+
+# Reshaping the array to 4-dims so that it can work with the Keras API
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+input_shape = (28, 28, 1)
+# Making sure that the values are float so that we can get decimal points after division
+x_train = x_train.astype('float32')
+x_test  = x_test.astype('float32')
+# # Normalizing the RGB codes by dividing it to the max RGB value.
+x_train /= 255.
+x_test /= 255.
+print('x_train shape:', x_train.shape)
+print('Number of images in x_train', x_train.shape[0])
+print('Number of images in x_test', x_test.shape[0])
+
+# load the trained CNN
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+# load weights into new model
+model.load_weights("model.h5")
+print("Loaded model from disk")
+
+#### BUBBLES
+# generate bubble mask
+nbMasks = 60000 # number of trials, 
+nbBubbles = 10 # number of bubbles
+dimOfinput = (28,28) # dimension of the input representation
+bubbleSize = [4, 4] # bubbles standards deviations in the different dimensions
+bubbleMasks = proise.generateBubbleMask(dimOfinput=dimOfinput, nbMasks=nbMasks, nbBubbles=nbBubbles, bubbleSize=bubbleSize)
+
+# generate probing samples
+probingMethod = 'bubbles' # choice of the probing method : bubbles or revcor
+samplesMethod = 'pseudoRandom' # choice of the method to generate probing samples trainSet or pseudoRandom (need x_test_set) or gaussianNoise
+nDim_pca = 50 # number of dimension to compute the PCA for the pseudo-random noise generation
+probingSamples = proise.generateProbingSamples(x_train_set = x_train, x_test_set = x_test, dimOfinput=dimOfinput, bubbleMasks = bubbleMasks, probingMethod = probingMethod, samplesMethod = samplesMethod, nDim_pca = nDim_pca)
+probingSamples = np.reshape(probingSamples,(probingSamples.shape[0],28,28,1))
+
+# compute predictions with the trained classifier
+y_pred = model.predict_classes(probingSamples)
+
+# compute discriminative maps
+discriminativeMaps = proise.ComputeDiscriminativeMaps(bubbleMasks,y_pred, dimOfinput=dimOfinput)
+
+# plot diagnostic maps
+fig, ax = plt.subplots(nrows=4, ncols=3)
+for iDigit in range(np.asarray(discriminativeMaps).shape[0]):
+	toPlot = np.asarray(discriminativeMaps)[iDigit,:]
+	plt.subplot(4,3,iDigit+1)
+	im = plt.imshow(np.reshape((toPlot),(28,28)) ,cmap='coolwarm') 
+	plt.title(str(np.unique(y_pred)[iDigit]))
+	plt.xticks([])
+	plt.yticks([])
+	plt.axis('off')	
+	plt.tight_layout()
+for x in ax.ravel():
+    x.axis("off")
+    plt.savefig('mnist_discriminative.png')
+plt.show()
+
+#### REVERSE CORRELATION
+# generate probing samples
+dimOfinput = (28,28) # dimension of the input representation
+probingMethod = 'revcor' # choice of the probing method : bubbles or revcor
+samplesMethod = 'pseudoRandom' # choice of the method to generate probing samples trainSet or pseudoRandom (need x_test_set) or gaussianNoise
+nbRevcorTrials = 60000 # number of probing samples for the reverse correlation (must be below the number of training sample if trainSet)
+nDim_pca = 50 # number of dimension to compute the PCA for the pseudo-random noise generation
+probingSamples = proise.generateProbingSamples(x_train_set = x_train, x_test_set = x_test, dimOfinput=dimOfinput, probingMethod = probingMethod, samplesMethod = samplesMethod, nDim_pca = nDim_pca, nbRevcorTrials = nbRevcorTrials)
+probingSamples = np.reshape(probingSamples,(probingSamples.shape[0],28,28,1))
+
+# compute predictions with the trained classifier
+y_pred = model.predict_classes(probingSamples)
+
+canonicalMaps = proise.ComputeCanonicalMaps(probingSamples,y_pred, dimOfinput=dimOfinput)
+
+# plot diagnostic maps
+fig, ax = plt.subplots(nrows=4, ncols=3)
+for iDigit in range(np.asarray(canonicalMaps).shape[0]):
+	toPlot = np.asarray(canonicalMaps)[iDigit,:]
+	plt.subplot(4,3,iDigit+1)
+	im = plt.imshow(np.reshape((toPlot),(28,28)) ,cmap='coolwarm') 
+	plt.title(str(np.unique(y_pred)[iDigit]))
+	plt.xticks([])
+	plt.yticks([])
+	plt.axis('off')	
+	plt.tight_layout()
+for x in ax.ravel():
+    x.axis("off")
+    plt.savefig('mnist_revcorr.png')
+plt.show()
+
+
---
+++ lib/proise.py
@@ -0,0 +1,80 @@
+# Basic functions for probing classifiers with revcor and bubbles
+#
+# Written by Etienne Thoret (2020)
+
+import numpy as np
+import random
+from scipy.ndimage import gaussian_filter
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+
+# compute the canonical map for a given category (category) from the probed samples (tab) and their classification item (y_pred)
+def ComputeOneCanonicalMap(tab,y_pred,category):
+	canonicalMap = (np.mean(np.asarray(tab[y_pred==category,:]),axis=0)-np.mean(np.asarray(tab[y_pred!=category,:]),axis=0))/np.sqrt(1/2*(np.std(np.asarray(tab[y_pred==category,:]),axis=0)**2+np.std(np.asarray(tab[y_pred!=category,:]),axis=0)**2))
+	return canonicalMap
+
+# compute the canonical maps for the different categories
+def ComputeCanonicalMaps(tab,y_pred,dimOfinput):
+	tabMaps = []
+	for iCateg in range(np.unique(y_pred).shape[0]):
+		category = np.unique(y_pred)[iCateg]
+		tabMaps.append(ComputeOneCanonicalMap(tab,y_pred,category))
+	return tabMaps
+
+# compute the discriminative map for a given category (category) from the probed samples (tab) and their classification item (y_pred)
+def ComputeOneDiscriminativeMap(tab,y_pred,category):
+	dprimeTab = np.sum(np.asarray(tab[y_pred==category,:]),axis=0) / (np.sum(np.asarray(tab),axis=0))
+	return dprimeTab
+
+# compute the discriminative maps for the different categories
+def ComputeDiscriminativeMaps(tab,y_pred,dimOfinput):
+	tabMaps = []
+	for iCateg in range(np.unique(y_pred).shape[0]):
+		category = np.unique(y_pred)[iCateg]
+		tabMaps.append(ComputeOneDiscriminativeMap(tab,y_pred,category))
+	return tabMaps
+
+# generate the bubble masks
+def generateBubbleMask(dimOfinput=(28,28), nbMasks=10, nbBubbles=10, bubbleSize=[4, 4]):
+	nFeatures = np.product(dimOfinput) # np.asarray(dimOfinput)[0]*np.asarray(dimOfinput)[1]
+	masks = np.random.rand(nbMasks,nFeatures)
+
+	for iSample in range(nbMasks):
+	    vec = masks[iSample,:]
+	    vec[:] = 0
+	    bubblePos = (np.floor(np.random.rand(1,nbBubbles)*nFeatures)).astype(int)
+	    vec[bubblePos] = 1
+	    vec = np.reshape(vec,dimOfinput) 
+	    vec = gaussian_filter(vec, sigma=bubbleSize)
+	    masks[iSample,:] = vec.flatten()
+	    masks[iSample,:] /= np.amax(masks[iSample,:])
+	    masks[iSample,:] -= np.amin(masks[iSample,:])
+	    masks[iSample,:] /= np.amax(masks[iSample,:])
+	return masks
+
+# generate probing samples from training set or with noise, or with pseudo-random noise from a testing set
+def generateProbingSamples(x_train_set = [], x_test_set = [], dimOfinput=(28,28), bubbleMasks = [], probingMethod = 'bubbles', samplesMethod = 'gaussianNoise', nDim_pca = 50, nbRevcorTrials = 1000):
+	if probingMethod == 'bubbles':
+		N_probing_samples = bubbleMasks.shape[0]
+		if samplesMethod == 'pseudoRandom':
+			pca_noise = PCA(n_components= nDim_pca, svd_solver='auto', whiten=True).fit(np.reshape(x_test_set,(x_test_set.shape[0],np.product(dimOfinput))))
+			probingSamples = pca_noise.inverse_transform(np.random.randn(bubbleMasks.shape[0],nDim_pca)) * bubbleMasks
+		elif samplesMethod == 'gaussianNoise':
+			probingSamples = np.random.randn(bubbleMasks.shape[0],bubbleMasks.shape[1]) * bubbleMasks
+		elif samplesMethod == 'trainSet':
+			probingSamples =  np.reshape(x_train_set,(x_train_set.shape[0],np.product(dimOfinput)))[0:bubbleMasks.shape[0],:] * bubbleMasks
+	elif probingMethod == 'revcor':
+		if samplesMethod == 'pseudoRandom':
+			pca_noise = PCA(n_components= nDim_pca, svd_solver='auto', whiten=True).fit(np.reshape(x_test_set,(x_test_set.shape[0],np.product(dimOfinput))))
+			probingSamples = pca_noise.inverse_transform(np.random.randn(nbRevcorTrials,nDim_pca))
+		elif samplesMethod == 'gaussianNoise':
+			probingSamples = np.random.randn(nbRevcorTrials,np.product(dimOfinput)) 
+		elif samplesMethod == 'trainSet':
+			probingSamples =  np.reshape(x_train_set,(x_train_set.shape[0],np.product(dimOfinput)))[0:nbRevcorTrials,:]  + np.random.randn(nbRevcorTrials,np.product(dimOfinput))
+	return probingSamples
+
+
+
+
+
